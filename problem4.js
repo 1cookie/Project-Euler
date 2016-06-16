41167fe31007e5090f7a42cc655b3203c54a131f
Andy <andy.cooksn@gmail.com>
2016-06-16 13:00:46 +0100

    Create problem4.js

---
+++ problem4.js
@@ -0,0 +1,21 @@
+function largestPalindrome(){
+  var stack = [];
+  var product = 0;
+  for(i=999;i>=100;i--){
+    for(j=i;j>=100;j--){
+       product = i*j;
+        if(checkPalindrome(product))
+          stack.push(product);
+       
+    } 
+  }
+  var largest = Math.max.apply(Math,stack);
+  return largest;
+}
+
+function checkPalindrome(str) {
+    var str = str+'';
+    return str == str.split('').reverse().join('');
+}
+
+largestPalindrome();
